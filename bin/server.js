7ebd8bbd5c0a93f37bc89cc11a98c38acc399361
keppy <keppy.rb@gmail.com>
2015-03-05 19:59:27 -0800

    Adds leveldb for GUID storage based on nick.
    Creates two endpoints for GUID saving and finding. Writes found GUID
    values to response. Only creates a GUID if the nick is unique, otherwise
    throws error.

--- bin/server.js
+++ bin/server.js
@@ -2,30 +2,65 @@ var app = require('http').createServer(handler);
 var fs = require('fs');
 var io = require('socket.io')(app);
 var hereGuids = fs.readdirSync(__dirname + '/../guids/here'); // This user's guids
-var mongoose = require('mongoose');
 var cp = require('child_process');
-
-
-// Mongoose init.
-mongoose.connect('mongodb://localhost:27017/');
-var User = mongoose.model('User', {guid: String, password: String});
-User.findOne({guid:'abc'});
+var level = require('level');
+var guidDb = level('./guids');
+var crypto = require('crypto');
 
 app.listen(8080);
 
+function createGuid () {
+    return crypto.randomBytes(20).toString('hex');
+}
+
+function storeGuid (guid, nick) {
+    guidDb.get(nick, function (err, value) {
+        if (err) {
+            guidDb.put(nick, guid, function (err) {
+                if (err) return console.log('Error placing key/value in db.', err);
+            });
+        } else {
+            return console.log('Nick for guid exists.');
+        }
+    });
+}
+
+function findGuid (nick, res) {
+    // Find a GUID and write it to a response.
+    guidDb.get(nick, function (err, value) {
+        if (err) return console.log('Nick not found.');
+        res.write(
+           JSON.stringify({guid: value}) 
+        );
+        return true;
+    });
+}
+
 function handler (req, res) {
 
-    if (req.method == 'POST' && req.url == "/new-guid"){
-      // Handles a guid authentication post
-      var body = '';
-      req.on('data', function (data) {
-          body += data;
-      });
-      req.on('end', function () {
-          res.writeHead(200, {'Content-Type': 'text/html'});
-          res.end('post received');
-          console.log(body);
-      });
+    if (req.method == 'POST' && req.url == '/new-guid'){
+        // Handles a guid authentication post
+        var body = undefined;
+        req.on('data', function (data) {
+            body = data;
+        });
+        req.on('end', function () {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('post received');
+            storeGuid(createGuid(), JSON.parse(body).nick);
+        });
+    }
+
+    if (req.method == 'POST' && req.url == '/existing-guid'){
+        var body = undefined;
+        req.on('data', function (data) {
+            body = data;
+        });
+        req.on('end', function () {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('post received');
+            findGuid(JSON.parse(body).nick, res);
+        });
     }
 
     if (req.url == "/favicon.ico"){   // handle requests for favico.ico
